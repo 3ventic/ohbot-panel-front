f0ef88fc7fa9c418841f4273eddc124c371ae410
3ventic <3ventic@gmail.com>
2016-02-26 01:17:28 +0200

    0first skeleton

---
+++ static/app.js
@@ -0,0 +1,101 @@
+(function () {
+    var path = window.location.hash;
+    var hasStorageSupport = false;
+    
+    window.onload = function () {
+        // Check localStorage support
+        try {
+            localStorage.setItem('test', 'test');
+            localStorage.removeItem('test');
+            hasStorageSupport = true;
+        }
+        catch (e) { }
+
+        var apiToken;
+    
+        // Get credentials
+        if (!hasStorageSupport || null === (apiToken = localStorage.getItem('apitoken'))) {
+            var qs = parseQueryString(path);
+            if ('access_token' in qs) {
+                window.location.hash = 'state' in qs ? qs['state'] : '';
+                path = window.location.hash;
+            }
+            else {
+                window.location.href = "https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&client_id=mndzi8dvtaknz32t2op18x0fcq71lm&redirect_uri=" +
+                encodeURIComponent("https://ohbot.3v.fi/panel/") + "&scope=&state=" + encodeURIComponent(path);
+            }
+        }
+        
+        if (apiToken) {
+            startApp(apiToken);
+        }
+        else {
+            apiRequest('token', null, function () {
+                try {
+                    var json = JSON.parse(this.responseText);
+                }
+                catch (e) {
+                    console.error(e);
+                    return;
+                }
+
+                if (json.status === 200) {
+                    startApp(json.token);
+                }
+                else {
+                    console.error("Token status " + json.status);
+                }
+            });
+        }
+    }
+    
+    function startApp(apiToken) {
+        apiRequest('me', null, function () {
+            try {
+                var channelData = JSON.parse(this.responseText);
+            }
+            catch (e) {
+                console.error("couldn't parse me response");
+                return;
+            }
+            initializeChannel(channelData);
+        });
+    }
+    
+    function initializeChannel(channelData) {
+        console.log(channelData);
+    }
+
+    function apiRequest(endpoint, data, callback, error) {
+        var url = "https://ohbot.3v.fi/api/v1/" + endpoint;
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', callback);
+        if (typeof error === "function") xhr.addEventListener('error', error);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Connection', 'close');
+
+        if (data) {
+            xhr.open("POST", url, true);
+            xhr.send(JSON.stringify(data));
+        }
+        else {
+            xhr.open("GET", url, true);
+            xhr.send();
+        }
+    }
+
+    function parseQueryString(qs) {
+        var parts = qs.split('&');
+        var values = {};
+        for (var i = 0; i < parts[i]; ++i) {
+            var keyval = parts[i].split('=');
+            if (keyval.length > 1) {
+                values[decodeURIComponent(keyval[0])] = decodeURIComponent(keyval[1]);
+            }
+            else {
+                values[decodeURIComponent(keyval[0])] = true;
+            }
+        }
+        return values;
+    }
+})();
